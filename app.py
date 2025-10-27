6d56007b696c42cef50642359073d0a1ee1a4d2c
AyushiVispute <ayushivispute5@gmail.com>
2025-10-27 21:49:07 +0530

    Prepare backend for Render deployment

--- app.py
+++ app.py
@@ -92,5 +92,6 @@ def transcribe_audio():
 
 
 if __name__ == "__main__":
-    port = int(os.getenv("PORT", 5000))
-    app.run(host="0.0.0.0", port=port, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=False)
+
